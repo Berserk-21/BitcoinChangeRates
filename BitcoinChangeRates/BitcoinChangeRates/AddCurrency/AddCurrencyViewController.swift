a793d3c275a93e5eb6518154a76fe0682f718b31
Berserk-21 <cryptogekkofr@protonmail.com>
2024-01-10 11:30:05 +0100

    Ajout d'un bouton cancel et d'un placeholder pour la searchBar

--- BitcoinChangeRates/BitcoinChangeRates/AddCurrency/AddCurrencyViewController.swift
+++ BitcoinChangeRates/BitcoinChangeRates/AddCurrency/AddCurrencyViewController.swift
@@ -7,10 +7,46 @@
 
 import UIKit
 
-class AddCurrencyViewController: UIViewController {
+final class AddCurrencyViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak private var searchBar: UISearchBar!
+    @IBOutlet weak private var tableView: UITableView!
+    
+    @IBOutlet weak private var cancelBarButton: UIBarButtonItem!
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupLayout()
+    }
+    
+    // MARK: - Setup Layout
+    
+    private func setupLayout() {
+        
+        setupNavBar()
+        setupSearchBar()
+    }
+    
+    private func setupNavBar() {
+        
+        let leftBarButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+        navigationItem.leftBarButtonItem = leftBarButton
+    }
+    
+    private func setupSearchBar() {
+        
+        searchBar.placeholder = "Search a currency.."
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func didTapCancel() {
+        
+        dismiss(animated: true)
     }
 }
